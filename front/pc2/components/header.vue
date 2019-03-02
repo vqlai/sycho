e0351130cbe2fce463fa2bf11431c909666565fc
vqlai <695850246@qq.com>
2019-03-02 15:18:58 +0800

    pc

---
+++ front/pc2/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h3>sycho</h3>
+    <p>欢喜勇猛，向死而生</p>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    computed: {
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  header {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ front/pc2/layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header/>
+    <Nav/>
+    <nuxt/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/header.vue'
+import Nav from '~/components/nav.vue'
+import Footer from '~/components/footer.vue'
+export default {
+  components: {
+    Header,
+    Nav,
+    Footer
+  },
+}
+</script>
+
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
