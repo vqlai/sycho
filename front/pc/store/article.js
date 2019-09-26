f6b81af63ee97b94b55992f6e9d43145bc9d5065
vqlai <695850246@qq.com>
2019-09-26 22:12:43 +0800

    update

--- front/pc/store/article.js
+++ front/pc/store/article.js
@@ -2,6 +2,7 @@
 export const state = () => ({
   // vue页面访问this.$store.state.article.articleList
   articleList: null,
+  articleHotList: null,
   article: null
 })
 
@@ -13,6 +14,9 @@ export const mutations = {
   SET_ARTICLE_List(state, data) {
     state.articleList = data
   },
+  SET_HOT_ARTICLE_List(state, data) {
+    state.articleHotList = data
+  },
   SET_ARTICLE(state, data) {
     state.article = data
   }
@@ -20,18 +24,26 @@ export const mutations = {
 
 export const actions = {
   getArticle({ commit }, params) {
-    console.log(params)
+    // console.log(params)
     return this.$axios.$get('/article', { params })
       .then(response => {
-        console.log(response)
+        // console.log(response)
         commit('SET_ARTICLE_List', response.data || {})
       })
   },
-  getArticleById({ commit }, params) {
-    console.info('getArticleById', params)
-    return this.$axios.$get(`/article2/${params.id}`)
+  getHotArticle({ commit }, params) {
+    console.info('getHotArticle', params)
+    return this.$axios.$get('/article', { params })
       .then(response => {
         console.log(response)
+        commit('SET_HOT_ARTICLE_List', response.data || {})
+      })
+  },
+  getArticleById({ commit }, params) {
+    // console.info('getArticleById', params)
+    return this.$axios.$get(`/article2/${params.id}`)
+      .then(response => {
+        // console.log(response)
         commit('SET_ARTICLE', response.data || {})
       })
   }
