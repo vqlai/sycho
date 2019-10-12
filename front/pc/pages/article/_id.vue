ce91db819dcd3baed07593519a0b723c46e2091a
vqlai <695850246@qq.com>
2019-10-12 23:21:23 +0800

    update

--- front/pc/pages/article/_id.vue
+++ front/pc/pages/article/_id.vue
@@ -8,12 +8,14 @@
       color: #333;
     }
     .share{
+      padding: 10px 0;
       @{deep} .share-base{
         display: flex;
         justify-content: center;
       }
     }
     .ant-carousel{
+      margin: 10px 0;
       width: 860px;
       height: 160px;
       text-align: center;
@@ -27,16 +29,7 @@
 <template>
   <section class="articel-detail">
     <h3 class="title">{{article.title}}</h3>
-    <!-- <div>
-      <image src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />
-    </div> -->
     <article v-html="article.content"></article>
-    <section>
-      <div>本文于   2019/6/19 上午  发布在  无色庵  分类下，当前已被围观  {{article.meta.views}}  次</div>
-      <div>相关标签： 诗和远方 、思考</div>
-      <div>永久地址： https://sycho.cn/article/{{article.id}}</div>
-      <div>版权声明： 自由转载-署名-非商业性使用   |   Sycho.cn</div>
-    </section>
     <a-carousel autoplay class="ad">
       <div>广告位1</div>
       <div>广告位2</div>
@@ -44,12 +37,108 @@
       <div>广告位4</div>
     </a-carousel>
     <share-box key="share" class="share"/>
+    <div>
+      <div>
+        本文于 {{moment(article.createTime).format('YYYY-MM-DD HH:mm:ss')}} 发布，作者 {{article.author}}
+      </div>
+      <div>
+        <span>当前已被围观 {{article.meta.views}} 次 </span>
+        <span @click.stop="onLike(article)">
+          <a-tooltip title="赞一个鸭">
+            <a-icon type="like" />
+          </a-tooltip>
+          <span style="padding-left: '8px';"> {{ article.meta.likes }} </span>
+        </span>
+        <span @click="onDislike(article)" >
+          <a-tooltip title="兄得请不要这样">
+            <a-icon type="dislike"/>
+          </a-tooltip>
+          <span style="padding-left: '8px';"> {{ article.meta.dislikes }} </span>
+        </span>
+        <a-tag color="#f50" @click="onShuang">赏</a-tag>
+      </div>
+      <div>相关标签： {{filterTags(article.tag)}}</div>
+      <div>永久地址： https://sycho.cn/article/{{article.id}}</div>
+      <div>版权声明： 自由转载-署名-非商业性使用 | 赛柯 | Sycho.cn</div>
+      <a-row :gutter="16" v-if="!isUser" style="padding:10px 0;">
+        <a-col :span="isEdit?6:7">
+          <a-input placeholder="昵称*" v-model="user.name">
+            <a-icon slot="prefix" type="user" />
+          </a-input>
+        </a-col>
+        <a-col :span="isEdit?6:7">
+          <a-input placeholder="邮箱*" v-model="user.email">
+            <a-icon slot="prefix" type="global" />
+          </a-input>
+        </a-col>
+        <a-col :span="isEdit?9:10">
+          <!-- <a-input placeholder="网址" v-model="user.url">
+            <a-icon slot="prefix" type="chrome" />
+          </a-input> -->
+          <a-input placeholder="网址" v-model="user.url">
+            <a-select slot="addonBefore" v-model="urlBefore" style="width: 90px">
+              <a-select-option value="https://">Https://</a-select-option>
+              <a-select-option value="http://">Http://</a-select-option>
+            </a-select>
+            <a-select slot="addonAfter" v-model="urlAfter" style="width: 80px">
+              <a-select-option value=".com">.com</a-select-option>
+              <a-select-option value=".cn">.cn</a-select-option>
+              <a-select-option value=".jp">.jp</a-select-option>
+              <a-select-option value=".org">.org</a-select-option>
+            </a-select>
+          </a-input>
+        </a-col>
+        <a-col :span="3" v-if="isEdit" @click="saveUser"><a-button icon="check" style="width:100%;"></a-button></a-col>
+      </a-row>
+      <a-row type="flex" justify="end" align="middle" style="padding:10px 0 0;" v-else>
+        {{user.name}}
+        <a-icon type="setting" style="padding: 0 10px;"/>
+        <a-dropdown v-model="dropdownVisible">
+          <a class="ant-dropdown-link" href="#"> 设置账户信息 </a>
+          <a-menu slot="overlay">
+            <a-menu-item key="1" @click="editUser">编辑信息</a-menu-item>
+            <a-menu-item key="2" @click="clearUser">清空信息</a-menu-item>
+          </a-menu>
+        </a-dropdown>
+      </a-row>
+      <a-row type="flex">
+        <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />
+        <div style="flex: 1;">
+          <div v-if="isReply">
+            <a-form-item style="padding: 0 0 6px;">
+              <a-input v-model="replyObj.name" disabled></a-input>
+            </a-form-item>
+            <a-form-item style="padding: 0 0 6px;">
+              <a-textarea v-model="replyObj.content" :rows="3" disabled></a-textarea>
+            </a-form-item>
+          </div>
+          <a-form-item>
+            <!-- :value="textareaValue" -->
+            <a-textarea :rows="4" v-model="commentText" placeholder="愿你的观点与众不同..."></a-textarea>
+          </a-form-item>
+          <a-form-item style="text-align:right;">
+            <a-button html-type="submit" :loading="submitting" @click="handleSubmit" type="primary" icon="smile"> 提交 </a-button>
+          </a-form-item>
+        </div>
+      </a-row>
+      <a-divider dashed />
+    </div>
+    <a-modal
+      title="你的支持是我坚持的源泉"
+      v-model="modalVisible"
+      :footer="null"
+      :maskClosable="true"
+      centered
+    >
+      <img src="/images/shang.jpg" style="width: 100%;height: auto;"/>
+    </a-modal>
   </section>
 </template>
 
 <script>
 // 文章详情
 import ShareBox from '~/components/share'
+import moment from 'moment'
 export default {
   name: 'ArticleDetail',
   head () {
@@ -63,6 +152,31 @@ export default {
   components: {
     ShareBox
   },
+  data() {
+    return{
+      moment,
+      modalVisible: false,
+      isUser: false, // 是否登录
+      user: {
+        name: '',
+        email: '',
+        url: '',
+        avatar: '',
+        color: ''
+      },
+      commentText: '',
+      urlBefore: 'Https://',
+      urlAfter: '.com',
+      isEdit: false,
+      dropdownVisible: false,
+      isReply: false, // 是否回复
+      replyObj: {
+        name: '',
+        content: ''
+      },
+      submitting: false
+    }
+  },
   validate ({ params }) {
     // 必须是number类型
     return /^\d+$/.test(params.id)
@@ -73,7 +187,7 @@ export default {
   computed: {
     article() {
       return this.$store.state.article.article
-    },
+    }
   },
   // data() {
   //   id: params.id
@@ -81,7 +195,97 @@ export default {
   mounted: function () {
     this.$nextTick(()=>{
       // console.log(this.$route.params.id)
+      // console.log(this.article)
+      let userStr = localStorage.getItem('user')
+      if(userStr){
+        this.isUser = true
+        this.user = JSON.parse(userStr)
+      }
     })
+  },
+  methods:{
+    clearUser() {},
+    editUser() {},
+    saveUser() {},
+    handleSubmit() {},
+    onLike(article) {
+      console.log(article)
+      if(!localStorage.getItem('user')){
+        this.$message.info('请先设置用户信息')
+        return false
+      }
+      let articleIds = JSON.parse(localStorage.getItem('articleIds'))
+      if(!articleIds||!articleIds.includes(article.id)){
+        let meta = {...article.meta}
+        meta.likes = meta.likes+1
+        this.$axios.patch('/articleLikes',{_id: article._id, meta}).then(res=>{
+          if(res.data.success){
+            let arr = articleIds || []
+            arr.push(article.id)
+            localStorage.setItem('articleIds', JSON.stringify(arr))
+            this.$message.info('点赞成功')
+            window.location.reload()
+          }else{
+            this.$message.info(res.data.msg)
+          }
+        })
+      }else{
+        this.$message.info('您已操作，请不要贪心哦')
+      }
+    },
+    onDislike(article) {
+      if(!localStorage.getItem('user')){
+        this.$message.info('请先设置用户信息')
+        return false
+      }
+      let articleIds = JSON.parse(localStorage.getItem('articleIds'))
+      if(!articleIds||!articleIds.includes(article.id)){
+        let meta = {...article.meta}
+        meta.dislikes = meta.dislikes+1
+        this.$axios.patch('/articleDislikes',{_id: article._id, meta}).then(res=>{
+          if(res.data.success){
+            let arr = articleIds || []
+            arr.push(article.id)
+            localStorage.setItem('articleIds', JSON.stringify(arr))
+            this.$message.info('吐槽成功')
+            window.location.reload()
+          }else{
+            this.$message.info(res.data.msg)
+          }
+        })
+      }else{
+        this.$message.info('您已操作，请不要贪心哦')
+      }
+    },
+    onShuang() {
+      this.modalVisible = true
+    },
+    filterTags(tags) {
+      let articleTags = [
+        { value: '1', label: 'javascript' }, 
+        { value: '2', label: 'css&css3' }, 
+        { value: '3', label: 'html&html5' },
+        { value: '4', label: 'vue' },
+        { value: '5', label: 'canvas' },
+        { value: '6', label: 'express' },
+        { value: '7', label: 'koa2' }, 
+        { value: '8', label: 'node' },
+        { value: '9', label: '小程序' },
+        { value: '10', label: 'app' }
+      ]
+      let arr = []
+      for(let i of tags.split(',')){
+        console.log(i)
+        for(let j of articleTags){
+          if(j.value == i){
+            console.log(j)
+            arr.push(j.label)
+            break
+          }
+        }
+      }
+      return arr.join('，')
+    }
   }
 }
 </script>
--- front/pc/pages/message.vue
+++ front/pc/pages/message.vue
@@ -1,7 +1,7 @@
 <style lang="less" scoped>
 @deep: ~'>>>';
   .message{
-    .guestbook-banner {
+    .message-banner {
       position: relative;
       margin-bottom: 20px;
       overflow: hidden;
@@ -48,7 +48,7 @@
     }
     .ant-comment{
       @{deep} .ant-comment-inner{
-        padding: 0;
+        padding: 0 0 10px;
       }
       .ant-row{
         margin: 0;
@@ -58,42 +58,42 @@
 </style>
 <template>
   <div class="message container">
-    <div class="guestbook-banner">
-      <img src="/images/guestbook.jpg">
+    <div class="message-banner">
+      <img src="/images/message.jpg">
       <span>此心光明 亦复何言</span>
     </div>
     <a-row :gutter="16" v-if="!isUser">
-      <a-col :span="isEdit?7:8">
-        <a-input placeholder="昵称*" v-model="user.nickname">
+      <a-col :span="isEdit?6:7">
+        <a-input placeholder="昵称*" v-model="user.name">
           <a-icon slot="prefix" type="user" />
         </a-input>
       </a-col>
-      <a-col :span="isEdit?7:8">
+      <a-col :span="isEdit?6:7">
         <a-input placeholder="邮箱*" v-model="user.email">
           <a-icon slot="prefix" type="global" />
         </a-input>
       </a-col>
-      <a-col :span="isEdit?7:8">
-        <a-input placeholder="网址" v-model="user.url">
-          <a-icon slot="prefix" type="chrome" />
-        </a-input>
+      <a-col :span="isEdit?9:10">
         <!-- <a-input placeholder="网址" v-model="user.url">
-          <a-select slot="addonBefore" defaultValue="Https://" style="width: 90px">
-            <a-select-option value="Https://">Https://</a-select-option>
-            <a-select-option value="Http://">Http://</a-select-option>
+          <a-icon slot="prefix" type="chrome" />
+        </a-input> -->
+        <a-input placeholder="网址" v-model="user.url">
+          <a-select slot="addonBefore" v-model="urlBefore" style="width: 90px">
+            <a-select-option value="https://">Https://</a-select-option>
+            <a-select-option value="http://">Http://</a-select-option>
           </a-select>
-          <a-select slot="addonAfter" defaultValue=".com" style="width: 80px">
+          <a-select slot="addonAfter" v-model="urlAfter" style="width: 80px">
             <a-select-option value=".com">.com</a-select-option>
             <a-select-option value=".cn">.cn</a-select-option>
             <a-select-option value=".jp">.jp</a-select-option>
             <a-select-option value=".org">.org</a-select-option>
           </a-select>
-        </a-input> -->
+        </a-input>
       </a-col>
       <a-col :span="3" v-if="isEdit" @click="saveUser"><a-button icon="check" style="width:100%;"></a-button></a-col>
     </a-row>
     <a-row type="flex" justify="end" align="middle" v-else>
-      {{user.nickname}}
+      {{user.name}}
       <a-icon type="setting" style="padding: 0 10px;"/>
       <a-dropdown v-model="dropdownVisible">
         <a class="ant-dropdown-link" href="#"> 设置账户信息 </a>
@@ -103,22 +103,26 @@
         </a-menu>
       </a-dropdown>
     </a-row>
-    <a-comment>
-      <a-avatar
-        slot="avatar"
-        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        alt="Han Solo"
-      />
-      <div slot="content">
+    <a-row type="flex">
+      <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />
+      <div style="flex:1;">
+        <div v-if="isReply">
+          <a-form-item style="padding: 0 0 6px;">
+            <a-input v-model="replyObj.name" disabled></a-input>
+          </a-form-item>
+          <a-form-item style="padding: 0 0 6px;">
+            <a-textarea v-model="replyObj.content" :rows="3" disabled></a-textarea>
+          </a-form-item>
+        </div>
         <a-form-item>
           <!-- :value="textareaValue" -->
-          <a-textarea :rows="4" v-model="messageText" placeholder="愿你的观点与众不同..."></a-textarea>
+          <a-textarea :rows="4" v-model="commentText" placeholder="愿你的观点与众不同..."></a-textarea>
         </a-form-item>
         <a-form-item style="text-align:right;">
           <a-button html-type="submit" :loading="submitting" @click="handleSubmit" type="primary" icon="smile"> 提交 </a-button>
         </a-form-item>
       </div>
-    </a-comment>
+    </a-row>
     <a-divider dashed />
     <a-row type="flex" justify="space-between" align="middle">
       <a-col>
@@ -127,91 +131,71 @@
         <a-tag color="#f50" @click="onShuang">赏</a-tag>
       </a-col>
       <a-col>
-        <a-radio-group default-value="a" button-style="solid">
-          <a-radio-button value="a">最新</a-radio-button>
-          <a-radio-button value="b">最热</a-radio-button>
+        <a-radio-group v-model="radioValue" button-style="solid" @change="onRadioChange">
+          <a-radio-button value="new">最新</a-radio-button>
+          <a-radio-button value="hot">最热</a-radio-button>
         </a-radio-group>
       </a-col>
     </a-row>
     <a-divider>评论区</a-divider>
-    <a-comment v-for="(item, index) in messageList.list" :key="index">
-      <!-- <span slot="actions">Reply to</span> -->
-      <template slot="actions">
-        <span> Windows 7 Chrome | 75 中国  -  杭州 </span>
-        <span>
-          <a-tooltip title="Like">
-            <a-icon
-              type="like"
-              :theme="action === 'liked' ? 'filled' : 'outlined'"
-              @click="like"
-            />
-          </a-tooltip>
-          <span style="padding-left: '8px';cursor: 'auto'">
-            {{ likes }}
-          </span>
-        </span>
-        <span>
-          <a-tooltip title="Dislike">
-            <a-icon
-              type="dislike"
-              :theme="action === 'disliked' ? 'filled' : 'outlined'"
-              @click="dislike"
-            />
-          </a-tooltip>
-          <span style="padding-left: '8px';cursor: 'auto'">
-            {{ dislikes }}
-          </span>
-        </span>
-        <span>Reply to</span>
-      </template>
-      <a slot="author">{{item.name}}</a>
-      <a-avatar
-        slot="avatar"
-        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        alt="Han Solo"
-      />
-      <p slot="content">{{item.content}}</p>
-      <a-tooltip slot="datetime" :title="moment(item.createDate).format('YYYY-MM-DD HH:mm:ss')">
-        <span>{{ moment(item.createDate).fromNow() }}</span>
-      </a-tooltip>
-      <!-- <a-comment>
-        <span slot="actions">Reply to</span>
-        <a slot="author">Han Solo</a>
-        <a-avatar
-          slot="avatar"
-          src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-          alt="Han Solo"
-        />
-        <p slot="content">We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure).</p>
-        <a-comment>
-          <span slot="actions">Reply to</span>
-          <a slot="author">Han Solo</a>
+    <div v-if ="messageList.list.length">
+      <a-row style="min-height: 160px;">
+        <a-comment v-for="(item, index) in messageList.list" :key="index">
+          <template slot="actions">
+            <span class="os" v-html="osParse(item.agent)" v-if="item.agent"></span>
+            <span class="ua" v-html="browserParse(item.agent)" v-if="item.agent"></span>
+            <span>
+              <a-tooltip title="赞一个鸭">
+                <a-icon type="like" :theme="action === 'liked' ? 'filled' : 'outlined'" @click="onLike(item)" />
+              </a-tooltip>
+              <span style="padding-left: '5px';cursor: 'auto'"> {{ item.likes }} </span>
+            </span>
+            <span>
+              <a-tooltip title="胸弟请不要这样">
+                <a-icon type="dislike" :theme="action === 'disliked' ? 'filled' : 'outlined'" @click="onDislike(item)" />
+              </a-tooltip>
+              <span style="padding-left: '5px';cursor: 'auto'"> {{ item.dislikes }} </span>
+            </span>
+            <span>
+              <a-tooltip title="说出来世界都是你的">
+                <a-icon type="rocket" :theme="action === 'disliked' ? 'filled' : 'outlined'" @click="onDislike(item)" />
+              </a-tooltip>
+              <span style="padding-left: '5px';cursor: 'auto'" @click="onReply(item)"> 回复 </span>
+            </span>
+          </template>
+          <a slot="author">{{item.name}}</a>
           <a-avatar
+            v-if="item.avator"
             slot="avatar"
             src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-            alt="Han Solo"
+            :alt="item.name"
           />
-          <p slot="content">We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure).</p>
+          <a-avatar shape="circle" size="large" :style="{backgroundColor: item.color, fontSize: '20px'}" slot="avatar">{{item.name.slice(0,1)}}</a-avatar>
+          <div slot="content">
+            <p style="color:#999;font-size: 12px;" v-if="item.isReply">
+              <span>回复</span>
+              <span style="margin: 0 6px;">{{'#'+ item.replyFloor}}</span>
+              <span>{{ '@' + item.replyName+'：'}}</span>
+            </p>
+            {{item.content}}
+          </div>
+          <a-tooltip slot="datetime" :title="moment(item.createDate).format('YYYY-MM-DD HH:mm:ss')">
+            <span>{{ moment(item.createDate).fromNow() }}</span>
+            <span style="margin-left: 16%;">{{'#'+item.id}}</span>
+          </a-tooltip>
         </a-comment>
-        <a-comment>
-          <span slot="actions">Reply to</span>
-          <a slot="author">Han Solo</a>
-          <a-avatar
-            slot="avatar"
-            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-            alt="Han Solo"
-          />
-          <p slot="content">We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure).</p>
-        </a-comment>
-      </a-comment> -->
-    </a-comment>
+      </a-row>
+      <a-row type="flex" justify="center" align="middle" style="padding-top: 20px;">
+        <a-pagination simple :pageSize="messageList.pagination.pageSize" :total="messageList.pagination.total" @change="onPageChange"/>
+      </a-row>
+    </div>
+    <div v-else style="text-align: center;padding-top: 30px;"> 暂无评论 </div>
     <a-modal
       title="你的支持是我坚持的源泉"
       v-model="modalVisible"
       :footer="null"
       :maskClosable="true"
-      centered
-    >
+      centered >
       <img src="/images/shang.jpg" style="width: 100%;height: auto;"/>
     </a-modal>
   </div>
@@ -219,6 +203,7 @@
 
 <script>
   import moment from 'moment'
+  import { browserParse, osParse } from '~/assets/js/ua-os-browser'
   export default {
     name: 'Message',
     head(){
@@ -237,18 +222,27 @@
         modalVisible: false,
         submitting: false,
         textareaValue: '',
-        isUser: false,
+        isUser: false, // 是否登录
         user: {
-          nickname: '',
+          name: '',
           email: '',
           url: '',
-          avatar: ''
+          avatar: '',
+          color: ''
         },
+        urlBefore: 'Https://',
+        urlAfter: '.com',
         isEdit: false,
-        messageText: '',
+        commentText: '',
         regexs: {
           email: /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/,
           url: /^((https|http):\/\/)+[A-Za-z0-9]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/
+        },
+        radioValue: 'new',
+        isReply: false, // 是否回复
+        replyObj: {
+          name: '',
+          content: ''
         }
       }
     },
@@ -259,6 +253,7 @@
     },
     mounted(){
       // console.info('user', localStorage.getItem('user'))
+      // 用户已经保存在storage则直接获取用户信息
       let userStr = localStorage.getItem('user')
       if(userStr){
         this.isUser = true
@@ -267,6 +262,8 @@
       console.info('getMessage', this.$store.state.message.messageList)
     },
     methods: {
+      browserParse,
+      osParse,
       // 编辑用户
       editUser(){
         this.isEdit = true
@@ -276,7 +273,7 @@
       clearUser(){
         this.isUser = false
         let user = {
-          nickname: '',
+          name: '',
           email: '',
           url: '',
           avatar: ''
@@ -290,15 +287,69 @@
         this.isEdit = false
         this.isUser = true
       },
-      like() {
-        this.likes = 1
-        this.dislikes = 0
-        this.action = 'liked'
+      // 点赞
+      onLike(item) {
+        if(!this.isUser){
+          window.scrollTo(0, 0)
+          this.$message.info('请先设置用户信息')
+          return false
+        }
+        let messageList = JSON.parse(localStorage.getItem('messageList'))
+        if(!messageList||!messageList.includes(item.id)){
+          this.$axios.patch('/messageLikes',{_id: item._id, likes: item.likes+1}).then(res=>{
+            if(res.data.success){
+              let arr = JSON.parse(localStorage.getItem('messageList')) || []
+              arr.push(item.id)
+              localStorage.setItem('messageList', JSON.stringify(arr))
+              let params = { currentPage: this.currentPage, pageSize: 10}
+              if(this.radioValue=='hot'){
+                params.hot = true
+              }
+              this.$store.dispatch('message/getMessage', params)
+            }else{
+              this.$message.info(res.data.msg)
+            }
+          })
+        }else{
+          this.$message.info('您已操作，请不要贪心哦')
+        }
       },
-      dislike() {
-        this.likes = 0
-        this.dislikes = 1
-        this.action = 'disliked'
+      // 吐槽
+      onDislike(item) {
+        if(!this.isUser){
+          window.scrollTo(0, 0)
+          this.$message.info('请先设置用户信息')
+          return false
+        }
+        let messageList = JSON.parse(localStorage.getItem('messageList'))
+        if(!messageList||!messageList.includes(item.id)){
+          this.$axios.patch('/messageDislikes',{_id: item._id, dislikes: item.dislikes+1}).then(res=>{
+            if(res.data.success){
+              let arr = JSON.parse(localStorage.getItem('messageList')) || []
+              arr.push(item.id)
+              localStorage.setItem('messageList', JSON.stringify(arr))
+              let params = { currentPage: this.currentPage, pageSize: 10}
+              if(this.radioValue=='hot'){
+                params.hot = true
+              }
+              this.$store.dispatch('message/getMessage', params)
+            }else{
+              this.$message.info(res.data.msg)
+            }
+          })
+        }else{
+          this.$message.info('您已操作，请不要贪心哦')
+        }
+      },
+      // 回复
+      onReply(item){
+        window.scrollTo(0, 0)
+        if(this.isUser){
+          this.isReply = true
+          this.replyObj = {...item}
+        }else{
+          this.$message.info('请先设置用户信息')
+        }
       },
       onShuang() {
         this.modalVisible = true
@@ -307,20 +358,60 @@
         let r = Math.floor(Math.random()*256)
         let g = Math.floor(Math.random()*256)
         let b = Math.floor(Math.random()*256)
-        return `rgb(${r}${g}${b})` 
+        return `rgb(${r},${g},${b})` 
       },
-      handleMenuClick() {
+      // 最新/最热切换
+      onRadioChange(e){
+        if(e.target.value === 'hot'){
+          this.$store.dispatch('message/getMessage', { currentPage: 1, pageSize: 10 ,hot: true})
+        }else{
+          this.$store.dispatch('message/getMessage', { currentPage: 1, pageSize: 10})
+        }
       },
+      handleMenuClick() {},
       handleSubmit() {
         console.log(this.user)
-        if(!this.user.nickname){ return this.$message.info('请输入昵称哟') }
+        if(!this.user.name){ return this.$message.info('请输入昵称哟') }
         if(!this.user.email){ return this.$message.info('请输入邮箱哟') }
         if (!this.regexs.email.test(this.user.email)) { return this.$message.info('请正确输入邮箱哟') }
         if(!this.user.url){ return this.$message.info('请输入网址哟') }
-        if (!this.regexs.url.test(this.user.url)) { return this.$message.info('请正确输入网址哟') }
-        if(!this.messageText){ return this.$message.info('请输入评论哟') }
-        localStorage.setItem('user', JSON.stringify(this.user))
-        this.user = true
+        // if (!this.regexs.url.test(this.user.url)) { return this.$message.info('请正确输入网址哟') }
+        if(!this.commentText){ return this.$message.info('请输入评论哟') }
+        this.user.url = `${this.urlBefore}${this.user.url}${this.urlAfter}`
+        this.user.color = this.isUser?this.user.color:this.randomColor()
+        let obj = {          
+          content: this.commentText
+        }
+        if(this.isReply){
+          obj.replyId = this.replyObj._id
+          obj.replyFloor = this.replyObj.id
+          obj.isReply = true
+          obj.replyName = this.replyObj.name
+          obj.replyContent = this.replyObj.content
+        }
+        this.submitting = true
+        this.$axios.post('/message',{...this.user, ...obj}).then(res=>{
+          console.log(res)
+          this.submitting = false
+          if(res.data.success){
+            this.isUser = true
+            this.isReply = false
+            localStorage.setItem('user', JSON.stringify(this.user))
+            this.commentText = ''
+            this.$message.info(res.data.msg)
+            // this.articleHotList = res.data.list
+          }else{
+            this.$message.info(res.data.msg)
+          }
+        })
+      },
+      onPageChange(page) {
+        this.currentPage = page
+        let params = { currentPage: page, pageSize: 10}
+        if(this.radioValue=='hot'){
+          params.hot = true
+        }
+        this.$store.dispatch('message/getMessage', params)
       }
     }
   }
