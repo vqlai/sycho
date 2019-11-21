dbe258eeeefada9953e94c3e513ff8c56381557e
vqlai <695850246@qq.com>
2019-11-21 22:34:57 +0800

    update

--- front/pc/pages/tech.vue
+++ front/pc/pages/tech.vue
@@ -1,31 +1,31 @@
 <style lang="less" scoped>
   @deep: ~'>>>';
   .tech{
-    .ant-carousel @{deep} .slick-slide {
-      text-align: center;
-      height: 160px;
-      line-height: 160px;
-      background: #364d79;
-      overflow: hidden;
-    }
-    .ant-carousel @{deep} .slick-slide h3 {
-      color: #fff;
-    }
-    .tech-banner {
+    .banner {
       position: relative;
       overflow: hidden;
       width: 100%;
-      max-height: 260px;
+      height: 260px;
       border: 0;
-      img {
-        width: 100%;
+      background: url('/images/tech.jpg') no-repeat;
+      background-size: cover;
+      background-position: center center;
+      text-align: center;
+      transition: all 1s;
+      &:hover {
+        transform: rotate(1deg) scale(0.95);
         transition: all 1s;
-        &:hover {
-          margin-top: -30px;
-          transform: scale(1.1);
-          transition: all 1s;
-        }
       }
+      // img {
+      //   width: 100%;
+      //   transition: all 1s;
+      //   &:hover {
+      //     // margin-top: -30px;
+      //     // transform: scale(1.1);
+      //     transform: rotate(-2deg) scale(1.1);
+      //     transition: all 1s;
+      //   }
+      // }
       span {
         position: absolute;
         right: 5%;
@@ -46,14 +46,8 @@
 </style>
 <template>
   <div class="tech container">
-    <!-- <a-carousel vertical>
-      <div><h3>1</h3></div>
-      <div><h3>2</h3></div>
-      <div><h3>3</h3></div>
-      <div><h3>4</h3></div>
-    </a-carousel> -->
-    <div class="tech-banner">
-      <img src="/images/tech.jpg">
+    <div class="banner">
+      <!-- <img src="/images/tech.jpg"> -->
       <span>填满一生，全是数字。</span>
     </div>
     <articleList :articleList="articleList" @onPagination="onPagination"></articleList>
