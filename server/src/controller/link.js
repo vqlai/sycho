9a4a6462782bf7a9dc3f37f88339bb16be57d711
vqlai <695850246@qq.com>
2019-12-20 09:24:09 +0800

    update

--- server/src/controller/link.js
+++ server/src/controller/link.js
@@ -1,6 +1,7 @@
 /**
  * Created by laiweiqang on 2019/04/26
  */
+
 // 链接控制器
 const Link = require('../model/link.js')
 const { handleSuccess, handleError } = require('../utils/handle')
@@ -20,7 +21,7 @@ class linkController {
     // 参数
     const querys = { name: new RegExp(keyword) }
     // 链接类型查询
-    if (['1', '2', '3', '4'].includes(type)) { querys.type = Number(type) }
+    if (['1', '2', '3', '4','5', '6', '7', '8', '9', '10'].includes(type)) { querys.type = Number(type) }
     // 查询
     const result = await Link
       .paginate(querys, options)
@@ -91,7 +92,6 @@ class linkController {
     const result = await Link
       .findByIdAndRemove(_id)
       .catch(err => {
-        // ctx.throw(500, '服务器内部错误-deleteLink错误！')
         throw new CustomError(500, '服务器内部错误')
         return false
       })
