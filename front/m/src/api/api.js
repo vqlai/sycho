3153d83691fba8553fa6c5f93eddada9e5c69205
vqlai <695850246@qq.com>
2020-01-19 22:44:20 +0800

    update

--- front/m/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import req from './request.js'
-
-export async function getInitData (params) {
-  return await req({
-    url: '/mall/runway/initialization.ihtml',
-    method: 'get',
-    params: { ...params }
-  })
-}
